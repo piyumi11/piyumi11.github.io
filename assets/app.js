39b0b086bd17418d611b3e1e1a1a617444f2262b
piyumi11 <piyumik11@gmail.com>
2023-09-16 22:25:11 +0530

    ts modificatins for 6 yards

--- assets/app.js
+++ assets/app.js
@@ -2,69 +2,13 @@
 js function customized for this application
 */
 
-    // Script to open and close sidebar
-    function w3_open() {
-      document.getElementById("mySidebar").style.display = "block";
-      document.getElementById("myOverlay").style.display = "block";
-    }
-
-    function w3_close() {
-      document.getElementById("mySidebar").style.display = "none";
-      document.getElementById("myOverlay").style.display = "none";
-    }
-
-    // Modal Image Gallery
-    function openImg(element) {
-      document.getElementById("img01").src = element.src;
-      document.getElementById("modal01").style.display = "block";
-      var captionText = document.getElementById("caption");
-      captionText.innerHTML = element.alt;
-    }
-/*
-js function customized for this application
-*/
-
-$(document).ready(function() {
-
-    // updates the sarees : start of d script
-
-    let table = document.getElementById("sarees");
-    let url = "assets/img/sarees/data.csv";
-
-    fetch(url)
-      .then(response => response.text())
-      .then(data => {
-        let rows = data.split("\n");
-        for (let i = 0; i < rows.length; i++) {
-
-            let cells = rows[i].split(",");
-
-            if(cells[0]!=="") {
-
-            var colDiv = document.createElement('div');
-            colDiv.className = 'col-md-3 six-display-container';
-
-            var img = document.createElement("img");
-            img.src = "assets/img/sarees/"+(i+1)+".jpg";
-            img.style = "width:100%;height:20em;object-fit: cover;";
-            img.setAttribute('onClick','openImg(this)');
-            img.alt = cells[0];
-
-            var imgText = document.createElement('div');
-            imgText.className = 'six-display-bottomright';
-            imgText.innerHTML = cells[0];
-
-            colDiv.appendChild(img);
-            colDiv.appendChild(imgText);
-            table.appendChild(colDiv);
-            }
-        }
-      })
-      .catch(error => console.log(error));
-    // updates the sarees : end of d script
-
-    });
-
+//open/zoom out the saree/product img
+function openImg(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+}
 
 $(document).ready(function(){
 
